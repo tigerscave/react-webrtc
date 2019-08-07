ca705512a428a894f666511436ab925cde612eee
cdra0624 <t.adaniya@adawarp.com>
2019-08-07 23:57:24 +0900

    feat(socket): enable to get socket and add user name

--- src/pages/operator-demo.js
+++ src/pages/operator-demo.js
@@ -1,11 +1,46 @@
 import React from "react";
+import io from 'socket.io-client';
+let socket = null
 
-const OperatorDemoPage = () => {
-  return (
-    <div>
-      <p>Operator Demo</p>
-    </div>
-  );
-};
+class OperatorDemoPage extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      socketId: "",
+      myName: ""
+    }
+
+    this.addName = () => {
+      const { myName } = this.state;
+      socket.emit("addName", myName);
+    }
+  }
+
+  componentDidMount() {
+    socket = io('https://telp-public-server.herokuapp.com/');
+
+    socket.on('connect', () => {
+      this.setState({
+        socketId: socket.id
+      })
+    })
+  }
+
+  render() {
+    const { socketId, myName } = this.state
+    return (
+      <div>
+        <h1>Operator Demo</h1>
+        <p>Your socket id is {socketId}</p>
+        <input
+          onChange={(e) => this.setState({ myName: e.target.value })}
+          onEnter={this.addName}
+        />
+        <button onClick={this.addName}>Join</button>
+      </div>
+    );
+  };
+}
 
 export default OperatorDemoPage;
