d3e1b3d80e54d9e6b51b7d00b0de94407d5ca111
cdra0624 <t.adaniya@adawarp.com>
2019-08-11 15:59:11 +0900

    feat(kanarobo): enable to change local video fps

--- src/components/kanarobotron-demo/video-list/video-container.js
+++ src/components/kanarobotron-demo/video-list/video-container.js
@@ -5,37 +5,57 @@ import { addVideoTrack as _addVideoTrack } from "../../../redux/reducers/tronRtc
 class VideoContainer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      frameRate: 1
+    };
     this.localVideoRef = React.createRef();
 
-    this.gotLocalVideoStream = mediaStream => {
+    this.setLocalVideoStream = mediaStream => {
       this.localVideoRef.current.srcObject = mediaStream;
 
       const { addVideoTrack } = this.props;
       addVideoTrack(mediaStream);
     };
+
+    this.onSelectChanged = e => {
+      const frameRate = e.target.value;
+      this.setState({ frameRate });
+      this.getUserVideoMedia(frameRate);
+    };
+
+    this.getUserVideoMedia = async frameRate => {
+      const { device } = this.props;
+      const mediaStream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          deviceId: device.deviceId,
+          frameRate: {
+            max: frameRate
+          }
+        }
+      });
+
+      this.setLocalVideoStream(mediaStream);
+    };
   }
 
   componentDidMount = async () => {
-    const { device } = this.props;
-    const mediaStream = await navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: {
-        deviceId: device.deviceId,
-        frameRate: {
-          max: 1
-        }
-      }
-    });
-    this.gotLocalVideoStream(mediaStream);
+    this.getUserVideoMedia(1);
   };
 
   render() {
     const { device } = this.props;
+    const { frameRate } = this.state;
     return (
       <li>
         <div>
           <video ref={this.localVideoRef} autoPlay />
           <p>デバイス名 : {device.label}</p>
+          <select value={frameRate} onChange={this.onSelectChanged}>
+            <option value={1}>1 FPS</option>
+            <option value={5}>5 FPS</option>
+            <option value={10}>10 FPS</option>
+          </select>
         </div>
         <style jsx>{`
           li {
