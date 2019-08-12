8d3f1cd52b5b4d0c5e90522b3f6ca7f7ccad1f5c
cdra0624 <t.adaniya@adawarp.com>
2019-08-12 14:20:21 +0900

    fix(devTools): add redux-devtool-extention library for ignore prod build

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,18 +2,17 @@ import { createBrowserHistory } from "history";
 import { createStore as _createStore, compose } from "redux";
 import reducers from "./reducers";
 import middlewares from "./middlewares";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
 export const history = createBrowserHistory();
 
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
+const devTools =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    : null;
 
 const createStore = () =>
-  _createStore(
-    reducers(history),
-    compose(
-      middlewares(history),
-      devTools
-    )
-  );
+  _createStore(reducers(history), composeWithDevTools(middlewares(history)));
 
 export default createStore;
